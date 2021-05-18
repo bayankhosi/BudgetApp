07b3eaa3cc430f3c488463f923c94057b265c729
Bayanda Dlamini <bayankhosi@gmail.com>
2021-05-18 18:48:53 +0200

    Inout_expenses

--- budget.py
+++ budget.py
@@ -22,11 +22,88 @@ def rec_income():
     new_value = value + cur_value
     income.cell(row=3 + income_type, column=3 + month).value = new_value
 
-    print("kak income")
-
 
 def rec_expense():
-    print("kak expense")
+    value = int(input("\nEnter Amount of Expense\n"))
+
+    expense_catagory = int(input("""Which Type of Catagory\n
+            [1] - Everyday Expenses
+            [2] - Home
+            [3] - Transport
+            [4] - Utilities
+            [5] - Education
+            [6] - Entertainment
+            [7] - Gifts
+        """))
+
+    if expense_catagory == 1:  # Everyday Expenses
+        expense_type = int(input("""Which Type of Expense\n
+            [1] - Groceries
+            [2] - Restaurants
+            [3] - Personal supplies
+            [4] - Clothes
+        """))
+        rw = 44 + expense_type
+
+    if expense_catagory == 2:  # Home
+        expense_type = int(input("""Which Type of Expense\n
+            [1] - Rent/mortgage
+        """))
+        rw = 69 + expense_type
+
+    if expense_catagory == 3:  # Transport
+        expense_type = int(input("""Which Type of Expense\n
+            [1] - Public Transit
+
+        """))
+        rw = 105 + expense_type
+
+    if expense_catagory == 4:  # Utilities
+        expense_type = int(input("""Which Type of Expense\n
+            [1] - Phone
+            [2] - TV
+            [3] - Internet
+            [4] - Electricity
+            [5] - Heat/Gas
+            [6] - Water
+        """))
+        rw = 124 + expense_type
+
+    if expense_catagory == 5:  # Education
+        expense_type = int(input("""Which Type of Expense\n
+            [1] - Tuition
+            [2] - Books
+        """))
+        rw = 22 + expense_type
+
+    if expense_catagory == 6:  # Entertainment
+        expense_type = int(input("""Which Type of Expense\n
+            [1] - Books
+            [2] - Concerts
+            [3] - Games
+            [4] - Hobbies
+            [5] - Movies
+            [6] - Music
+            [7] - Outdoor activities
+            [8] - Photography
+            [9] - Sport
+            [10] - Theatre/plays
+            [11] - TV
+            [12] - Other
+        """))
+        rw = 29 + expense_type
+
+    if expense_catagory == 7:  # Gifts
+        expense_type = int(input("""Which Type of Expense\n
+            [1] - Gifts
+            [2] - Charity
+            [3] - Other
+        """))
+        rw = 55 + expense_type
+
+    cur_value = expenses.cell(row=rw, column=3 + month).value
+    new_value = value + cur_value
+    expenses.cell(row=rw, column=3 + month).value = new_value
 
 
 def data():
@@ -38,7 +115,7 @@ while loop == 2:
     choice = int(
         input("Choose Operation:\n1. Record Expense  2. Record Income  3. View Data\n"))
     if choice == 1:
-        rec_income()
+        rec_expense()
     elif choice == 2:
         rec_income()
     elif choice == 3:
